696348e4852630ba028371f60d0c4c600fcb6df6
IanaCris <ianasousati@gmail.com>
2020-04-16 00:21:14 -0300

    Revert Last Commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
-import Header from './components/Header/index.js';
-import Main from './pages/main';
-
 function App() {
+  const [repositories, setRepositories] = useState([]);
+  
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
+  async function handleAddRepository() {
+    const response = await api.post('repositories', {
+      title: `Iana`,
+      url: 'https://github.com/ianacris',
+      techs: ['Node', 'ReactJS']
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+  }
+
+  async function handleRemoveRepository(id) {
+    await api.delete(`repositories/${id}`);
+
+    setRepositories(repositories.filter(
+      repository => repository.id !== id
+    ))
+  }
 
   return (
-    <>
-      <Header title="JSGit"/>
-      <Main />    
-    </>
+    <div>
+      <ul data-testid="repository-list">
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
+
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
   );
 }
 
--- src/components/Header/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import './styles.css';
-
-export default function Header({title}) {
-  return (
-    <header id="main-header">{title}</header>
-  );
-}
